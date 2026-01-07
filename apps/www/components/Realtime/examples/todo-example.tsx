8815747bb201dff73a2b8e060eef73ee13973704
vchavkov82 <vchavkov@gmail.com>
2026-01-07 10:57:25 +0000

    clean up errors

--- apps/www/components/Realtime/examples/todo-example.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function RealtimeExample() {
-  return (
-    <div className="p-4 bg-gray-100 rounded">
-      <p className="text-gray-600">Realtime example disabled - Supabase removed</p>
-    </div>
-  )
-}
